deb8cb5a1f0e578e3ec2042e04974bc54af24c4a
Alexander Ushanov <flawless@last-try.org>
2018-12-23 04:37:52 +0300

    Added simple interactive to run tests

---
+++ flaskel.el
@@ -0,0 +1,10 @@
+(defun flaskel-test (&optional buf-name)
+  (interactive)
+  (let (old-point
+        (buf-name (or buf-name "*flask-test*")))
+    (unless (comint-check-proc buf-name)
+      (with-current-buffer (get-buffer-create buf-name)
+        (unless (zerop (buffer-size)) (setq old-point (point)))
+        (pop-to-buffer buf-name)
+        (shell-command "docker-compose exec -T web flask test" buf-name)))
+    (when old-point (push-mark old-point))))
